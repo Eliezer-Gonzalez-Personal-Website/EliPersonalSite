24404063449a32165be5a5cdafcc3130ba553945
eliezergonza <eliezergonza@gmail.com>
2023-04-17 00:56:20 -0500

    Added Contact Page

--- personal_site/javascript.js
+++ personal_site/javascript.js
@@ -1,4 +1,26 @@
+const form = document.querySelector("#form")
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxLZTlgHBunzjDAyvzTFB9AAQZS2vGT4hSZKlfMDVMQrrHEQwKDYO2-fo05BNfFegYWVg/exec'
+
+form.addEventListener('submit', e => {
+  submitButton.disabled = true
+  e.preventDefault()
+  let requestBody = new FormData(form)
+  fetch(scriptURL, { method: 'POST', body: requestBody})
+    .then(response => {
+       alert('Success!', response)
+       submitButton.disabled = false
+      })
+    .catch(error => {
+    alert('Error!', error.message)
+      submitButton.disabled = false
+
+    }
+    )
+})
+
 function myFunction() {
     alert("Copyright 2023. Created by Eli Gonzalez");
   }
+
 
